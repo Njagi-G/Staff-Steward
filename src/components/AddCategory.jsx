6a676527204af16e79f1246fead38257e36e4b1e
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-12 12:37:48 +0300

    Created AddCategory Component

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AddCategory() {
+  return (
+    <div>
+        <div>
+            <h3>Add Category</h3>
+        </div>
+    </div>
+  )
+}
+
+export default AddCategory;
