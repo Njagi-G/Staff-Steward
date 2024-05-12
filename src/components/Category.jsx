e9a25a82100bfb726cade579a9336ca3cf465ef5
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-12 12:39:08 +0300

    Created Category Component

---
+++ src/components/Category.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Category() {
+  return (
+    <div className='px-5 mt-3'>
+        <div className='d-flex justify-content-center'>
+            <h3>Category Class</h3>
+        </div>
+        <Link to='/dashboard/add-category' className='btn btn-success' >Add Category</Link>
+        
+      
+    </div>
+  )
+}
+
+export default Category; 
