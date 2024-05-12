d39b42fefdb7eff10b14ec9f7162dfe0b9199ef7
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-12 12:47:37 +0300

    Added Routes to add-category, category, employee and profile

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Login from './components/Login';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Dashboard from './components/Dashboard';
 import Home from './components/Home';
+import Employee from './components/Employee';
+import Category from './components/Category';
+import Profile from './components/Profile';
+import AddCategory from './components/AddCategory';
 
 function App() {
 
@@ -13,6 +17,11 @@ function App() {
       <Route path='/adminlogin' element={<Login />}></Route>
       <Route path='/dashboard' element={<Dashboard />}>
         <Route path='' element={<Home />} ></Route>
+        <Route path='/dashboard/employee' element={<Employee />} ></Route>
+        <Route path='/dashboard/category' element={<Category/>} ></Route>
+        <Route path='/dashboard/profile' element={<Profile />} ></Route>
+        <Route path='/dashboard/add-category' element={<AddCategory />} ></Route>
+        
       </Route>
     </Routes>
     
