1077b624c1e55193d23db37ab933d1951ad534eb
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-13 06:51:47 +0300

    Add skeleton of landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
     <div>
-      Home
+      <div className="p-3 d-flex justify-content-around mt-3">
+        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+          <div className="text-center pb-1">
+            <h4>Admin</h4>
+          </div>
+          <hr />
+          <div className="">
+            <h5>Total: </h5>
+          </div>
+        </div>
+        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+          <div className="text-center pb-1">
+            <h4>Employee</h4>
+          </div>
+          <hr />
+          <div className="">
+            <h5>Total: </h5>
+          </div>
+        </div>
+        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+          <div className="text-center pb-1">
+            <h4>Salary</h4>
+          </div>
+          <hr />
+          <div className=""></div>
+            <h5>Total: </h5>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
