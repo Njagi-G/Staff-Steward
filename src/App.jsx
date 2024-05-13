aed7b692d0922f12d3b1c686e327fe40e7f41f2d
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-13 04:53:42 +0300

    Add form for editing employee details and a route to Edit Employee page on clicking Edit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './components/Login';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard';
 import Home from './components/Home';
 import Employee from './components/Employee';
@@ -9,26 +9,27 @@ import Department from './components/Department';
 import Profile from './components/Profile';
 import AddDepartment from './components/AddDepartment';
 import AddEmployee from './components/AddEmployee';
+import EditEmployee from './components/EditEmployee';
 
 
 function App() {
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/adminlogin' element={<Login />}></Route>
-      <Route path='/dashboard' element={<Dashboard />}>
-        <Route path='' element={<Home />} ></Route>
-        <Route path='/dashboard/employee' element={<Employee />} ></Route>
-        <Route path='/dashboard/department' element={<Department/>} ></Route>
-        <Route path='/dashboard/profile' element={<Profile />} ></Route>
-        <Route path='/dashboard/add-department' element={<AddDepartment />} ></Route>
-        <Route path='/dashboard/add-employee' element={<AddEmployee />} ></Route>
-      </Route>
-    </Routes>
-    
+      <Routes>
+        <Route path='/adminlogin' element={<Login />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}>
+          <Route path='' element={<Home />} ></Route>
+          <Route path='/dashboard/employee' element={<Employee />} ></Route>
+          <Route path='/dashboard/department' element={<Department />} ></Route>
+          <Route path='/dashboard/profile' element={<Profile />} ></Route>
+          <Route path='/dashboard/add-department' element={<AddDepartment />} ></Route>
+          <Route path='/dashboard/add-employee' element={<AddEmployee />} ></Route>
+          <Route path='/dashboard/edit-employee/:id' element={<EditEmployee />}></Route>
+        </Route>
+      </Routes>
+
     </BrowserRouter>
-   
   )
 }
 
--- src/components/EditEmployee.jsx
+++ src/components/EditEmployee.jsx
@@ -1,14 +1,124 @@
-
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function EditEmployee() {
-    const {id} = useParams()
-  return (
-    <div>
-      EditEmployee
-    </div>
-  )
+    const { id } = useParams()
+
+    const [employee, setEmployee] = useState({
+        name: "",
+        email: "",
+        password: "",
+        salary: "",
+        address: "",
+        department_id: "",
+        image: "",
+    });
+
+    const [department, setDepartment] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/auth/department')
+            .then(result => {
+                if (result.data.Status) {
+                    setDepartment(result.data.Result)
+                } else {
+                    alert(result.data.Error)
+                }
+            })
+            .catch(err => console.log(err))
+
+        axios.get('http://localhost:3000/auth/employee/' + id)
+            .then(result => {
+                setEmployee({
+                    ...employee,
+                    name: result.data.Result[0].name,
+                    email: result.data.Result[0].email,
+                    address: result.data.Result[0].address,
+                    salary: result.data.Result[0].salary,
+                })
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    return (
+        <div className="d-flex justify-content-center align-items-center mt-3">
+            <div className="p-3 rounded w-50 border">
+                <h2 className="text-center"><strong>Edit Employee</strong></h2>
+                <form className="row g-1">
+                    <div className="col-12">
+                        <label for="inputName" className="form-label">
+                            <strong>Name</strong>
+                        </label>
+                        <input
+                            className="form-control rounded-0"
+                            id="inputName"
+                            type="text"
+                            placeholder="Enter Employee Name"
+                            value={employee.name}
+                            onChange={(e) => setEmployee({ ...employee, name: e.target.value })}
+                        />
+                    </div>
+                    <div className="col-12">
+                        <label for="inputEmail14" className="form-label">
+                            <strong>Email</strong></label>
+                        <input
+                            type="email"
+                            className="form-control rounded-0"
+                            id="inputEmail14"
+                            placeholder="Enter Email Address"
+                            autoComplete="off"
+                            value={employee.email}
+                            onChange={(e) => setEmployee({ ...employee, email: e.target.value })}
+                        />
+                    </div>
+                    <div className='col-12'>
+                        <label for="inputSalary" className="form-label"><strong>Salary</strong></label>
+                        <input
+                            type="text"
+                            className="form-control rounded-0"
+                            id="inputSalary"
+                            placeholder="Enter Salary"
+                            autoComplete="off"
+                            value={employee.salary}
+                            onChange={(e) => setEmployee({ ...employee, salary: e.target.value })}
+                        />
+                    </div>
+                    <div className="col-12">
+                        <label for="inputAddress" className="form-label">
+                            <strong>Address</strong>
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control rounded-0"
+                            id="inputAddress"
+                            placeholder="Street/Estate, City"
+                            autoComplete="off"
+                            value={employee.address}
+                            onChange={(e) => setEmployee({ ...employee, address: e.target.value })}
+                        />
+                    </div>
+                    <div className="col-12">
+                        <label for="department" className="form-label">
+                            <strong>Select Department</strong>
+                        </label>
+                        <select
+                            name="department"
+                            id="department"
+                            className="form-select"
+                            onChange={(e) => setEmployee({ ...employee, department_id: e.target.value })}
+                        >{department.map((d) => {
+                            return <option value={d.id}>{d.name}</option>;
+                        })}
+                        </select>
+                    </div>
+                    <div className="col-12">
+                        <button type="submit" className="btn btn-primary w-100">Edit Employee</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default EditEmployee;
