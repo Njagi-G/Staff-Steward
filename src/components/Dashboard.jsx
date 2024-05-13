3b156ed84401c2475eaf147fd62d438c666b62ef
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-13 10:09:48 +0300

    Add Sign-out functionality

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,21 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import axios from "axios";
 
 function Dashboard() {
+  const navigate = useNavigate()
+  axios.defaults.withCredentials = true
+  function handleSignout() {
+    axios.get('http://localhost:3000/auth/signout')
+    .then(result => {
+      if(result.data.Status) {
+        navigate('/adminlogin')
+
+      }
+    })
+
+  }
   return (
     <div className="container-fluid">
       <div className="row flex-nowrap">
@@ -56,7 +69,7 @@ function Dashboard() {
                   <span className="ms-2 d-none d-sm-inline">Profile</span>
                 </Link>
               </li>
-              <li className="w-100">
+              <li className="w-100" onClick={handleSignout}>
                 <Link className="nav-link px-0 align-middle text-white">
                   <i className="fs-4 b-power ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">Sign Out</span>
