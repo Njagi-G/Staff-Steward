0a743050aebe28eb23b95be6de5901e625b3df02
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-13 03:28:05 +0300

    Add Link to EditEmployee

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -47,10 +47,8 @@ function Employee() {
                   <td>{e.address}</td>
                   <td>{e.salary}</td>
                   <td>
-                    <button className="btn btn-info btn-sm me-2">Edit</button>
+                    <Link to={`/dashboard/edit-employee/`+e.id}className="btn btn-info btn-sm me-2">Edit</Link>
                     <button className="btn btn-warning btn-sm">Delete</button>
-
-
                   </td>
                 </tr>
               ))
