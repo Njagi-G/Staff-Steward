344e714c2bf55fc0379baaa58251ed8995d99d36
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-13 07:12:36 +0300

    Display total admins in organization

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
+  const [adminTotal, setAdminTotal] = useState()
+  const [employeeTotal, setEmployeeTotal] = useState()
+  const [salaryTotal, setSalaryTotal] = useState()
+
+  useEffect(() => {
+    adminCount();
+
+
+  }, [])
+
+  function adminCount() {
+    axios.get('http://localhost:3000/auth/admin-count')
+    .then(result => {
+      if(result.data.Status) {
+        setAdminTotal(result.data.Result[0].admin)
+      }
+    })
+
+  }
   return (
     <div>
       <div className="p-3 d-flex justify-content-around mt-3">
@@ -9,8 +29,9 @@ function Home() {
             <h4>Admin</h4>
           </div>
           <hr />
-          <div className="">
-            <h5>Total: </h5>
+          <div className="d-flex justify-content-around">
+            <h5>Total:</h5>
+            <h5>{adminTotal} </h5>
           </div>
         </div>
         <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
