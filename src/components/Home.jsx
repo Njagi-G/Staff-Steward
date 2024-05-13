93f0013e24b08b3c09be03bde4c260219f0a557a
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-13 08:12:56 +0300

    Create home page showing total personnel, total salaries paid, list of admins

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Home() {
-  const [adminTotal, setAdminTotal] = useState()
-  const [employeeTotal, setEmployeeTotal] = useState()
-  const [salaryTotal, setSalaryTotal] = useState()
+  const [adminTotal, setAdminTotal] = useState(0)
+  const [employeeTotal, setEmployeeTotal] = useState(0)
+  const [salaryTotal, setSalaryTotal] = useState(0)
+  const [admins, setAdmins] = useState([])
 
   useEffect(() => {
     adminCount();
-
-
+    employeeCount();
+    salaryCount();
+    adminRecords();
   }, [])
 
   function adminCount() {
@@ -19,38 +21,96 @@ function Home() {
         setAdminTotal(result.data.Result[0].admin)
       }
     })
+  }
+
+  function employeeCount() {
+    axios.get('http://localhost:3000/auth/employee-count')
+    .then(result => {
+      if(result.data.Status) {
+        setEmployeeTotal(result.data.Result[0].employee)
+      }
+    })
+  }
+
+  function salaryCount() {
+    axios.get('http://localhost:3000/auth/salary-count')
+    .then(result => {
+      if(result.data.Status) {
+        setSalaryTotal(result.data.Result[0].salary)
+      }
+    })
+  }
+
+  function adminRecords() {
+    axios.get('http://localhost:3000/auth/admin-records')
+    .then(result => {
+      if(result.data.Status) {
+        setAdmins(result.data.Result)
+      } else {
+        alert(reesult.data.Error)
+      }
+    })
 
   }
+
   return (
     <div>
       <div className="p-3 d-flex justify-content-around mt-3">
         <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
           <div className="text-center pb-1">
-            <h4>Admin</h4>
+            <h4>Admins</h4>
           </div>
           <hr />
-          <div className="d-flex justify-content-around">
+          <div className="d-flex justify-content-between">
             <h5>Total:</h5>
             <h5>{adminTotal} </h5>
           </div>
         </div>
         <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
           <div className="text-center pb-1">
-            <h4>Employee</h4>
+            <h4>Employees</h4>
           </div>
           <hr />
-          <div className="">
-            <h5>Total: </h5>
+          <div className="d-flex justify-content-between">
+            <h5>Total:</h5>
+            <h5>{employeeTotal} </h5>
           </div>
         </div>
         <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
           <div className="text-center pb-1">
-            <h4>Salary</h4>
+            <h4>Salaries</h4>
           </div>
           <hr />
-          <div className=""></div>
+          <div className="d-flex justify-content-between">
             <h5>Total: </h5>
+            <h5>KES {salaryTotal} </h5>
+          </div>
         </div>
+      </div>
+      <div className="mt-4 px-5 pt-3">
+        <h3>List of Admins</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              admins.map(a => (
+                <tr>
+                  <td>{a.email}</td>
+                  <td>
+                    <button className="btn btn-info btn-sm me-2">Edit</button>
+                    <button className="btn btn-warning btn-sm " >Delete</button>
+                  </td>
+                </tr>
+              ))
+            }
+
+          </tbody>
+        </table>
       </div>
     </div>
   );
